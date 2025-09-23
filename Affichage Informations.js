80850bcfe444e4d6704f08a02bb8999e5a27fdf5
tourabikhadija <italia.khadija2000@gmail.com>
2025-09-23 16:13:29 +0100

    first challange

---
+++ Affichage Informations.js
@@ -0,0 +1,15 @@
+const prompt = require("prompt-sync")();
+
+let Nom =prompt ("Entrer votre Nom: ");
+let prenom =prompt ("Entrer votre prenom: ");
+let age =prompt ("Entrer votre age: ");
+let sexe =prompt ("Entrer votre sexe: ");
+let tel =prompt ("Entrer votre Numéro_de_téléphone: ");
+
+
+
+console.log ("Nom:" + Nom);
+console.log ("prenom:" + prenom);
+console.log ("age:" + age);
+console.log ("sexe:" + sexe);
+console.log ("Numéro_de_téléphone:" + tel);
