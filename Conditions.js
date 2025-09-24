127cdc9bb4631254e557cc146c5ed6d6b5e9557e
tourabikhadija <italia.khadija2000@gmail.com>
2025-09-24 16:01:13 +0100

    test

--- Conditions.js
+++ Conditions.js
@@ -1,9 +1,8 @@
 const prompt = require("prompt-sync")();
-
 let nombre = prompt ("Entrer votre numbre: ");
 if(nombre % 2 === 0) {
 
     console.log("le nombre est pair");
  }else{
     console.log("le nombre est impair");
-  }  
+  } 
